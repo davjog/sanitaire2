fa7702933ccf6931fba510da0bf7ca44a54b2913
davjog <davjoguet@free.fr>
2026-06-10 20:53:46 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,9 +1,26 @@
-const CACHE = 'carnet-sanitaire-v1';
-self.addEventListener('install', e =>
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(['./'])))
-);
-self.addEventListener('fetch', e =>
+const CACHE = 'carnet-sanitaire-v2';
+const FILES = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).catch(() => caches.match('./')))
-  )
-);
+    caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
